33d2979bc87367c15ff2891b3f45f6132d011d81
Enimalojd <enderston8@gmail.com>
2023-12-14 18:40:56 +0400

    add routes for notes

--- routes/notes.js
+++ routes/notes.js
@@ -1,6 +1,23 @@
 const express = require("express");
 const router = express.Router();
 const { auth } = require("../middleware/auth");
-const { isAdmin } = require("../middleware/adminCheck")
+const { isAdmin } = require("../middleware/adminCheck");
+const {
+  all,
+  target,
+  addNote,
+  deleteNote,
+  changeNote,
+} = require("../controllers/notes");
+// Ве заметки для модератора
+router.get("/", auth, isAdmin, all);
+// Личные заметки пользователя
+router.get("/:id", auth, target);
+//Добавление заметки
+router.post("/add", auth, isAdmin, addNote);
+//Удаление заметки
+router.post("/remove/:id", auth, isAdmin, deleteNote);
+//Изменение заметки
+router.put("edit/:id", auth, isAdmin, changeNote);
 
-router.get("/", auth,isAdmin  () => );
+module.exports = router;
