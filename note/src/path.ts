212e57290b77067242132d818e506397b447e9cf
Enimalojd <enderston8@gmail.com>
2023-12-14 19:20:29 +0400

    add dev files

---
+++ note/src/path.ts
@@ -0,0 +1,9 @@
+export const Path = {
+  home: "/",
+  noteAdd: "/note/add",
+  noteEdit: "/note/edit",
+  note: "/note",
+  status: "/status",
+  login: "/login",
+  register: "/register",
+} as const;
